6262a6a10a2bd16865fde47077e230a5c9a662f9
Davi Quirino <160233282+daviiq@users.noreply.github.com>
2025-12-11 19:16:29 -0300

    Adição da interface Coletador + Adição de vida do pobre coitado do Aldeão
    Implemented Coletador interface in Aldeao class and added coletar method.

--- app/src/main/java/ifsc/joe/domain/impl/Aldeao.java
+++ app/src/main/java/ifsc/joe/domain/impl/Aldeao.java
@@ -1,16 +1,22 @@
 package ifsc.joe.domain.impl;
 
+import ifsc.joe.Interfaces.Coletador;
 import ifsc.joe.enums.Direcao;
 import javax.swing.*;
 import java.awt.*;
 import java.util.Objects;
 
-public class Aldeao extends Personagem {
+public class Aldeao extends Personagem implements Coletador {
     public static final String NOME_IMAGEM = "aldeao";
 
     public Aldeao(int x, int y) {
         // vida, ataque, defesa
         super(x, y, false, null, 100, 0, 5);
+    }
+
+    @Override
+    public void coletar() {
+        //Eu coleto se tiver tempo :)
     }
 
     // Desenha o Aldeão
@@ -20,6 +26,7 @@ public class Aldeao extends Personagem {
         this.icone = this.carregarImagem(NOME_IMAGEM + (atacando ? "2" : ""));
         // desenhando de fato a imagem no pai
         g.drawImage(this.icone, this.posX, this.posY, painel);
+        desenharVida(g);
     }
     /**
      * Metodo auxiliar para carregar uma imagem do disco
